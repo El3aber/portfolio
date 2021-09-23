f4975b74de246a39711b20882bd45666553e6166
El3aber <abadi444449@gmail.com>
2021-09-23 10:06:30 +0200

    anything

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,6 +8,17 @@ export default function Footer() {
   const {isDark} = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
+    <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("Made with ❤️ by El3aber ")}
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          Theme by{" "}
+          <a href="https://github.com/El3aber">
+            developerFolio
+          </a>
+        </p>
+      </div>
     </Fade>
   );
 }
